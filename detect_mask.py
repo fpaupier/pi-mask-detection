efeb31b6090844440dc0c869dfcd20bcf792ab4a
François Paupier <francois.paupier@gmail.com>
2020-08-02 08:56:25 +0200

    add: base pipeline

---
+++ detect_mask.py
@@ -0,0 +1,13 @@
+def main():
+    # Get camera feed
+    pass
+
+    # Apply face detection
+    pass
+
+    # Apply mask / no mask binary classifier
+    pass
+
+
+if __name__ == '__main__':
+    main()
