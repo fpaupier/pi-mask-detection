80353316742d8c207476c4b61f4db0653eff6e72
François Paupier <francois.paupier@gmail.com>
2020-08-26 10:14:24 +0200

    rm: draw faces

--- detect_mask.py
+++ detect_mask.py
@@ -47,17 +47,6 @@ def make_interpreter(model_file):
         ])
 
 
-def draw_objects(draw, objs, labels):
-    """Draws the bounding box and label for each object."""
-    for obj in objs:
-        bbox = obj.bbox
-        draw.rectangle([(bbox.xmin, bbox.ymin), (bbox.xmax, bbox.ymax)],
-                       outline='red')
-        draw.text((bbox.xmin + 10, bbox.ymin + 10),
-                  '%s\n%.2f' % (labels.get(obj.id, obj.id), obj.score),
-                  fill='red')
-
-
 def get_image(img_path):
     """
     Get the next image to process for the pipeline.
@@ -120,11 +109,7 @@ def main():
         print('  score: ', face.score)
         print('  bbox:  ', face.bbox)
 
-    if args.output:
-        image = image.convert('RGB')
-        draw_objects(ImageDraw.Draw(image), faces, labels)
-        image.save(args.output)
-
+    image = image.convert('RGB')
     # For each face in the image crop around the ROI and detect if mask or not mask
 
     # Apply mask / no mask classifier
