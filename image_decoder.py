6d5b07d13bbc58aefbb0b2ed60ed0fa051bb6087
François Paupier <francois.paupier@gmail.com>
2020-08-26 10:35:07 +0200

    add: basic image decoder

---
+++ image_decoder.py
@@ -0,0 +1,19 @@
+import io
+
+from PIL import Image
+
+import alert_pb2
+
+
+def main():
+    recovered_alert = alert_pb2.Alert()
+    with open("serializedAlert", 'rb') as fd:
+        recovered_alert.ParseFromString(fd.read())
+
+    print(recovered_alert)
+    img = Image.open(io.BytesIO(recovered_alert.image.data))
+    img.show()
+
+
+if __name__ == "__main__":
+    main()
